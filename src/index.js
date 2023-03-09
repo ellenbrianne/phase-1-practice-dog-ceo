992ae007bd447e926bcc7f06e054b2a090f5f013
Ellen Bennett <bennette831@gmail.com>
2023-03-08 22:43:20 -0700

    completed challenge 1 & 2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('%c HI', 'color: firebrick')
+// Challenge 1
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl)
+    .then((resp) => resp.json())
+    .then((data) => {
+        data.message.forEach(element => {
+            let imgContainer = document.getElementById("dog-image-container")
+            img = document.createElement("img")
+            img.src = `${element}`
+            imgContainer.appendChild(img)
+        })
+    })
+    .catch((error) => console.log(error));
+
+
+// Challenge 2
+
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+fetch(breedUrl) 
+    .then(resp => resp.json())
+    .then(data => {
+        let breedObject = data.message
+        console.log(breedObject)
+        for (let key in breedObject) {
+            let li = document.createElement("li")
+            li.textContent = key
+            let breedList = document.getElementById("dog-breeds")
+            breedList.appendChild(li)
+        }
+    })
