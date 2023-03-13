4129e15f8ae6aee16145d412b82cacaf7b9e4418
Ellen Bennett <bennette831@gmail.com>
2023-03-13 00:27:59 -0600

    completed challenges 3 & 4

--- src/index.js
+++ src/index.js
@@ -19,15 +19,36 @@ fetch(imgUrl)
 
 const breedUrl = "https://dog.ceo/api/breeds/list/all";
 
+let breedList = document.getElementById("dog-breeds")
+
+function renderBreeds (breed) {
+    let li = document.createElement("li")
+            li.textContent = breed
+            breedList.appendChild(li)
+
+            // Challenge 3
+
+            li.addEventListener("click", (e) => {
+                li.style.color = "purple"
+            })
+        }
+
 fetch(breedUrl) 
     .then(resp => resp.json())
     .then(data => {
-        let breedObject = data.message
-        console.log(breedObject)
-        for (let key in breedObject) {
-            let li = document.createElement("li")
-            li.textContent = key
-            let breedList = document.getElementById("dog-breeds")
-            breedList.appendChild(li)
-        }
-    })
+
+        // Challenge 4
+        
+        let breedArray = Object.keys(data.message)
+        breedArray.forEach(renderBreeds)
+
+            let letter = document.querySelector("select")
+            letter.addEventListener("change", (e) => {
+                let filteredBreeds = breedArray.filter((breed) => {
+                    return e.target.value === breed[0]
+                })
+
+                breedList.replaceChildren()
+                filteredBreeds.forEach(renderBreeds)
+            })
+        })
